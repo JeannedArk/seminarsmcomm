a3b946c7d7473cbc6ff035f1a9aaf1a372c9082c
Timo Gühring <s9tigueh@stud.uni-saarland.de>
2017-01-26 19:10:00 +0100

    bgescripting.py: socket conn attemps

--- bgescripting.py
+++ bgescripting.py
@@ -6,7 +6,15 @@ import asyncio
 # UDP setup
 UDP_IP = "127.0.0.1"
 UDP_PORT = 23000
+# Die Konstante AF_INET steht für Adressfamilie Internet.
+# Die Konstante SOCK_DGRAM steht für das UDP-Protokoll.
+# Dies sind optionale Eingaben für eine Socket-Instanz.
+# Voreingestellt ist AF_INET und als Protokoll SockStream, dies steht für
+# ein TPC-socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
 
 """
 https://docs.python.org/3/library/asyncio-eventloop.html
@@ -21,3 +29,14 @@ def keypressed():
     print("key pressed")
 
 def update():
+    while 1:    # Endlosschleife
+        data, addr = sock.recvfrom( 1024 )
+        # Puffer-Größe ist 1024 Bytes.
+        # Die Puffergröße muss immer eine Potenz
+        # von 2 sein
+        print("empfangene Nachricht:", data)
+        print()"Clientadresse:", addr)         # Adresse besteht aus IP und Port
+
+def send(msg):
+    print("Message:", msg)
+    sock.sendto(msg, (UDP_IP, UDP_PORT))

6d5f8ad8d7142fb0c012621d1ae825f0e9547ab4
Timo Gühring <s9tigueh@stud.uni-saarland.de>
2017-01-27 13:58:48 +0100

    Conn tests work

--- UDPConnTest.py
+++ UDPConnTest.py
@@ -4,39 +4,16 @@ import socket
 
 #Example taken from
 #https://docs.python.org/3/library/asyncio-eventloop.html
-
+#https://wiki.python.org/moin/UdpCommunication
 
 
 # UDP setup
 UDP_IP = "127.0.0.1"
 UDP_PORT = 23000
 
-# Create a pair of connected file descriptors
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
 
-
-
-rsock, wsock = socketpair()
-loop = asyncio.get_event_loop()
-
-def reader():
-    data = rsock.recv(100)
-    print("Received:", data.decode())
-    # We are done: unregister the file descriptor
-    loop.remove_reader(rsock)
-    # Stop the event loop
-    loop.stop()
-
-# Register the file descriptor for read event
-loop.add_reader(rsock, reader)
-
-# Simulate the reception of data from the network
-loop.call_soon(wsock.send, 'abc'.encode())
-
-# Run the event loop
-loop.run_forever()
-
-# We are done, close sockets and the event loop
-rsock.close()
-wsock.close()
-loop.close()
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print("received message:", data)

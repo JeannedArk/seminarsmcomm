76c9e2ec796a5a62a92dc92df9ffe625e8a82157
Timo Gühring <timo.guehring@googlemail.com>
2017-02-10 14:49:36 +0100

    Some improvements

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DIRECTORY="/Applications/blender-2.*/blender.app/Contents/MacOS/blender"
 
-# Start Visual SceneMaker
-java -classpath "./VisualSceneMaker/bld/VisualSceneMaker3.jar" de.dfki.vsm.SceneMaker3 >> /dev/null
 if [ -f $DIRECTORY ]; then
+    # Start Visual SceneMaker
+    java -classpath "./VisualSceneMaker/bld/VisualSceneMaker3.jar" de.dfki.vsm.SceneMaker3 >> /dev/null &
     # Start Blender with Anna file
     open $DIRECTORY "./Anna/Anna-v04.blend"
 else

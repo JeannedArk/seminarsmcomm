803bf72eb0280f0a3e2511da700f347c99f3e6bc
Timo Gühring <timo.guehring@googlemail.com>
2017-02-11 13:23:21 +0100

    Some improvements

--- start.sh
+++ start.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-DIRECTORY="/Applications/blender-2.*/blender.app/Contents/MacOS/blender"
+APPLICATION1="/Applications/blender-2.*/blender.app/Contents/MacOS/blender"
+APPLICATION2="/Applications/blender.app/Contents/MacOS/blender"
 
-if [ -f $DIRECTORY ]; then
+if [ -f $APPLICATION1 ]; then
     # Start Visual SceneMaker
     java -classpath "./VisualSceneMaker/bld/VisualSceneMaker3.jar" de.dfki.vsm.SceneMaker3 >> /dev/null &
     # Start Blender with Anna file
-    open $DIRECTORY "./Anna/Anna-v04.blend"
+    open $APPLICATION1 "./Anna/Anna-v04.blend"
+elif [ -f $APPLICATION2 ]; then
+    # Start Visual SceneMaker
+    java -classpath "./VisualSceneMaker/bld/VisualSceneMaker3.jar" de.dfki.vsm.SceneMaker3 >> /dev/null &
+    # Start Blender with Anna file
+    open $APPLICATION2 "./Anna/Anna-v04.blend"
 else
-    echo "Blender path not found. Expected Blender installation path in $DIRECTORY"
+    echo "Blender path not found. Expected Blender installation path in $APPLICATION1 or $APPLICATION2"
 fi
